65c496d39cf726e36ff214e3a05fd7a0ac23ea24
Vansh Raj sharma <vanshatri777@gmail.com>
2025-12-23 18:17:53 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountID=22323;
+let accountNAME="vansh11";
+var accountPASSWORD=33424;
+accountCITY="delhi";
+
+accountPASSWORD=86883;
+accountNAME="shivam";
+accountCITY="mumbai";
+console.table([accountPASSWORD,accountNAME,accountCITY]);
+/*
+prefer not to use var because of block scope and function scope
+*/
